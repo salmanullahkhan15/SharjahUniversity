38e0f62a333645224e492937b997e7fb794c9abb
Salman Ullah Khan <salman@texpo.com>
2019-10-24 14:27:24 +0500

    final commit

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -10,7 +10,7 @@ export class TabsPage {
   iconOne: any = 'assets/icon/icon-white.svg';
   IconOneSelected: any = 'assets/icon/icon-orange.svg';
   // IconOneSelected: any = 'assets/icon/one-icon.png';
-  oneSelected: number = 1
+  oneSelected: number = 3
   constructor() { }
   selecTab(n) {
     this.oneSelected = n
